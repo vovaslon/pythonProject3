c048f48e28ed0637cc4aede745c43d18a721539f
vovaslon <vovaslon34@gmail.com>
2023-10-01 20:50:42 +0300

    add function.py

--- function.py
+++ function.py
@@ -23,12 +23,18 @@ def print_5(dict):
 
 def hide_cardnumbers(card):
     numbers = card.split()[-1]
-    return (f'{numbers[0:4]} {numbers[4:6]}** **** {numbers[-4:]}')
+    return f'{card.split()[0]} {numbers[0:4]} {numbers[4:6]}** **** {numbers[-4:]}'
 
 
 def hide_accountnumbers(account):
     numbers = account.split()[-1]
-    return (f'**{numbers[-4:]}')
+    return f'**{numbers[-4:]}'
 
 
+def date_(date):
+    date_f = date.split('T')[0]
+    return f'{date_f[-2:]}.{date_f[-5:-3]}.{date_f[0:4]}'
 
+# f'{date_()} {***["description"]}\n{}'
+
+print(hide_cardnumbers("MasterCard 8847384717023026"))
